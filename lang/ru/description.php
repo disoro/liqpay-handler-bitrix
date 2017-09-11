50ab8308250078fdd72795b81d5779bf98588cf2
disoro <disoro@mail.com>
2017-09-11 16:39:18 +0300

    Add files via upload

---
+++ lang/ru/description.php
@@ -0,0 +1,40 @@
+<?
+/**
+ * Liqpay Payment Handler
+ *
+ * Customized liqpay payment handler.
+ * It uses the extended capability.
+ *
+ * @category    Liqliqpaycheckout
+ *
+ * @author      Dmytro Sorokin <disoro@mail.com>
+ *
+ * @license     Code and contributions have 'MIT License'
+ *              More details: https://github.com/disoro/liqpay-handler-bitrix/blob/master/LICENSE
+ *
+ * @link        GitHub Repo:  https://github.com/disoro/liqpay-handler-bitrix
+ *
+ * @version     1.0.0
+ *
+ * EXTENSION INFORMATION
+ *
+ * 1C-Bitrix        17.0
+ * LIQPAY API       https://www.liqpay.ua/documentation/ru
+ *
+ */
+
+$MESS['SALE_HPS_LP_PUBLIC_KEY'] = 'Публичный ключ';
+$MESS['SALE_HPS_LP_PRIVATE_KEY'] = 'Cекретный ключ';
+$MESS['SALE_HPS_LP_PATH_TO_SERVER_URL'] = 'Путь к файлу-обработчику результата';
+$MESS['SALE_HPS_LP_PATH_TO_RESULT_URL'] = 'Путь к файлу для возврата покупателя на сайт';
+$MESS['SALE_HPS_LP_ORDER_ID'] = 'Номер оплаты';
+$MESS['SALE_HPS_LP_CURRENCY'] = 'Валюта счета';
+$MESS['SALE_HPS_LP_SHOULD_PAY'] = 'К оплате';
+$MESS['SALE_HPS_LP_PAYMENT_TYPE'] = 'Способ оплаты';
+$MESS['SALE_HPS_LP_TYPE_LIQPAY'] = 'Кабинет liqpay';
+$MESS['SALE_HPS_LP_TYPE_CARD'] = 'Оплата картой';
+$MESS['SALE_HPS_LP_TYPE_PRIVAT24'] = 'Кабинет приват24';
+$MESS['SALE_HPS_LP_TYPE_CASH'] = 'Наличными';
+$MESS['SALE_HPS_LP_ACTION'] = 'URL Liqpay API';
+$MESS['SALE_HPS_LP_TEST'] = 'Тестовый режим';
+$MESS['SALE_HPS_LP_LIQPAY_CHECKOUT'] = 'liqpay checkout';
